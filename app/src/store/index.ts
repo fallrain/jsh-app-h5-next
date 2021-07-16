2df895a68dc330b6a119939697aa21b25a5bc7cd
王广开 <416266475@qq.com>
2021-07-16 15:31:05 +0800

    toolkit

--- app/src/store/index.ts
+++ app/src/store/index.ts
@@ -1,31 +1,13 @@
-import {
-  createStore,
-  applyMiddleware,
-  compose,
-  Middleware
-} from 'redux';
-import thunk from 'redux-thunk';
+import { configureStore } from '@reduxjs/toolkit';
 import reducers from './redusers';
 
-declare global {
-  interface Window {
-    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
-  }
-}
+// declare global {
+//   interface Window {
+//     __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+//   }
+// }
+const store = configureStore({
+  reducer: reducers,
+});
 
-const composeEnhancers = typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-    // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
-  }) : compose;
-
-const middleWares: Middleware[] = [
-  thunk
-];
-const enhancer = composeEnhancers(
-  applyMiddleware(...middleWares)
-  // other store enhancers if any
-);
-const store = createStore(reducers, enhancer);
 export default store;
